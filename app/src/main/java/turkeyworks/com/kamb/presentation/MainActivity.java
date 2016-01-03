03a0df7e5ce5a060dc820b40a6b1b506ff0f9d26
Karl Wiens <kwiens@visualspection.com>
2016-01-03 12:26:24 -0600

    Cleaned up release code

--- app/src/main/java/turkeyworks/com/kamb/presentation/MainActivity.java
+++ app/src/main/java/turkeyworks/com/kamb/presentation/MainActivity.java
@@ -112,8 +112,8 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         if (id == R.id.action_settings) {
-            Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
-            startActivity(intent);
+         //   Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+           // startActivity(intent);
             return true;
         }
         else if (id == R.id.action_generate) {
